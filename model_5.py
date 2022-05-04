75702044cf484f22f5d3bf875decdedfbccb8740
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-05-04 07:49:19 -0400

    Update model_5.py

--- model_5.py
+++ model_5.py
@@ -16,36 +16,37 @@ def Upsample(tensor, size):
                output_shape=size)(tensor)
     return y
 
-
 def ASPP(tensor, filters):
     '''atrous spatial pyramid pooling'''
     dims = K.int_shape(tensor)
 
+    split_dim = dims[3] // 4
+
     y_pool = AveragePooling2D(pool_size=(
-        dims[1], dims[2]))(tensor)
-    y_pool = Conv2D(filters=filters, kernel_size=1, padding='same', use_bias=False)(y_pool)
+        dims[1], dims[2]))(tensor[:, :, :, 0*split_dim:1*split_dim])
+    y_pool = Conv2D(filters=split_dim, kernel_size=1, padding='same', use_bias=False)(y_pool)
     y_pool = BatchNormalization()(y_pool)
     y_pool = Activation('relu',)(y_pool)
 
     y_pool = Upsample(tensor=y_pool, size=[dims[1], dims[2]])
 
-    y_1 = Conv2D(filters=filters, kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(tensor)
+    y_1 = Conv2D(filters=split_dim, kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(tensor[:, :, :, 1*split_dim:2*split_dim])
     y_1 = BatchNormalization()(y_1)
     y_1 = Activation('relu')(y_1)
 
-    y_6 = Conv2D(filters=filters, kernel_size=3, dilation_rate=6, padding='same', use_bias=False)(tensor)
+    y_6 = Conv2D(filters=split_dim, kernel_size=3, dilation_rate=6, padding='same', use_bias=False)(tensor[:, :, :, 2*split_dim:3*split_dim])
     y_6 = BatchNormalization()(y_6)
     y_6 = Activation('relu')(y_6)
 
-    y_12 = Conv2D(filters=filters, kernel_size=3, dilation_rate=12, padding='same', use_bias=False)(tensor)
+    y_12 = Conv2D(filters=split_dim, kernel_size=3, dilation_rate=12, padding='same', use_bias=False)(tensor[:, :, :, 3*split_dim:4*split_dim])
     y_12 = BatchNormalization()(y_12)
     y_12 = Activation('relu')(y_12)
 
-    y_18 = Conv2D(filters=filters, kernel_size=3, dilation_rate=18, padding='same', use_bias=False)(tensor)
-    y_18 = BatchNormalization()(y_18)
-    y_18 = Activation('relu')(y_18)
+    # y_18 = Conv2D(filters=filters, kernel_size=3, dilation_rate=18, padding='same', use_bias=False)(tensor)
+    # y_18 = BatchNormalization()(y_18)
+    # y_18 = Activation('relu')(y_18)
 
-    y = concatenate([y_pool, y_1, y_6, y_12, y_18])
+    y = concatenate([y_pool, y_1, y_6, y_12])
 
     y = Conv2D(filters=filters, kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(y)
     y = BatchNormalization()(y)
@@ -57,34 +58,36 @@ def attention_ASPP(tensor, filters):
     '''atrous spatial pyramid pooling'''
     dims = K.int_shape(tensor)
 
+    split_dim = dims[3] // 4
+
     y_pool = AveragePooling2D(pool_size=(
-        dims[1], dims[2]))(tensor)
+        dims[1], dims[2]))(tensor[:, :, :, 0*split_dim:1*split_dim])
     y_pool = DepthwiseConv2D(kernel_size=1, padding='same', use_bias=False)(y_pool)
     y_pool = BatchNormalization()(y_pool)
     y_pool = Activation('relu',)(y_pool)
 
     y_pool = Upsample(tensor=y_pool, size=[dims[1], dims[2]])
 
-    y_1 = DepthwiseConv2D(kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(tensor)
+    y_1 = Conv2D(filters=split_dim, kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(tensor[:, :, :, 1*split_dim:2*split_dim])
     y_1 = BatchNormalization()(y_1)
     y_1 = Activation('relu')(y_1)
 
-    y_6 = DepthwiseConv2D(kernel_size=3, dilation_rate=6, padding='same', use_bias=False)(tensor)
+    y_6 = Conv2D(filters=split_dim, kernel_size=3, dilation_rate=6, padding='same', use_bias=False)(tensor[:, :, :, 2*split_dim:3*split_dim])
     y_6 = BatchNormalization()(y_6)
     y_6 = Activation('relu')(y_6)
 
-    y_12 = DepthwiseConv2D(kernel_size=3, dilation_rate=12, padding='same', use_bias=False)(tensor)
+    y_12 = Conv2D(filters=split_dim, kernel_size=3, dilation_rate=12, padding='same', use_bias=False)(tensor[:, :, :, 3*split_dim:4*split_dim])
     y_12 = BatchNormalization()(y_12)
     y_12 = Activation('relu')(y_12)
 
-    y_18 = DepthwiseConv2D(kernel_size=3, dilation_rate=18, padding='same', use_bias=False)(tensor)
-    y_18 = BatchNormalization()(y_18)
-    y_18 = Activation('relu')(y_18)
+    # y_18 = DepthwiseConv2D(kernel_size=3, dilation_rate=18, padding='same', use_bias=False)(tensor)
+    # y_18 = BatchNormalization()(y_18)
+    # y_18 = Activation('relu')(y_18)
 
-    y = concatenate([y_pool, y_1, y_6, y_12, y_18])
+    y = concatenate([y_pool, y_1, y_6, y_12])
 
     y = Conv2D(filters=filters, kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(y)
-    #y = DepthwiseConv2D(kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(y)
+    # y = DepthwiseConv2D(kernel_size=1, dilation_rate=1, padding='same', use_bias=False)(y)
     y = BatchNormalization()(y)
     y = tf.reduce_mean(y, -1, keepdims=True)
     y = tf.nn.sigmoid(y)
@@ -102,34 +105,45 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     h_1 = tf.keras.layers.BatchNormalization()(h_1)
     h_1 = tf.keras.layers.ReLU()(h_1)
 
-    h = tf.pad(h_1, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=2, use_bias=False)(h)    # downsample
-    h = tf.keras.layers.BatchNormalization()(h)
-    h = tf.keras.layers.ReLU()(h)
+    # h = tf.pad(h_1, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
+    # h = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=2, use_bias=False)(h)    # downsample
+    # h = tf.keras.layers.BatchNormalization()(h)
+    # h = tf.keras.layers.ReLU()(h)
+    h = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2)(h_1) # downsample
 
     h_att_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h)
     h_att_2 = tf.keras.layers.BatchNormalization()(h_att_2)
-    h_att_2 = tf.keras.layers.ReLU()(h_att_2) + h
+    h_att_2 = tf.keras.layers.ReLU()(h_att_2)
 
     h_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False)(h_att_2)
     h_2 = tf.keras.layers.BatchNormalization()(h_2)
     h_2 = tf.keras.layers.ReLU()(h_2)
 
-    h = tf.pad(h_2, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=2, use_bias=False)(h)    # downsample
-    h = tf.keras.layers.BatchNormalization()(h)
-    h = tf.keras.layers.ReLU()(h)
+    # h = tf.pad(h_2, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
+    # h = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=2, use_bias=False)(h)    # downsample
+    # h = tf.keras.layers.BatchNormalization()(h)
+    # h = tf.keras.layers.ReLU()(h)
+    h = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2)(h_2) # downsample
 
     h_att_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h)
     h_att_3 = tf.keras.layers.BatchNormalization()(h_att_3)
-    h_att_3 = tf.keras.layers.ReLU()(h_att_3) + h
+    h_att_3 = tf.keras.layers.ReLU()(h_att_3)
     
     h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(h_att_3)
     h_3 = tf.keras.layers.BatchNormalization()(h_3)
     h_3 = tf.keras.layers.ReLU()(h_3)
 
-    h = tf.pad(h_3, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=2, use_bias=False)(h)    # downsample
+    # h = tf.pad(h_3, [[0,0],[1,1],[1,1],[0,0]], "REFLECT")
+    # h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=2, use_bias=False)(h)    # downsample
+    # h = tf.keras.layers.BatchNormalization()(h)
+    # h = tf.keras.layers.ReLU()(h)
+    h = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2)(h_3) # downsample
+
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding="same", use_bias=False)(h)
+    h = tf.keras.layers.BatchNormalization()(h)
+    h = tf.keras.layers.ReLU()(h)
+
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding="same", use_bias=False)(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU()(h)
     encoder_2 = h
@@ -179,6 +193,10 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU()(h)
 
+    h = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False)(h)
+    h = tf.keras.layers.BatchNormalization()(h)
+    h = tf.keras.layers.ReLU()(h)
+
     h = tf.keras.layers.Conv2D(filters=classes, kernel_size=1)(h) # object
     decoder_output_1 = h
     #########################################################################################################################
@@ -195,6 +213,6 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     #decoder_2 = tf.keras.layers.ReLU()(decoder_2)
     #decoder_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(decoder_2)
     #decoder_2 = tf.keras.layers.BatchNormalization()(decoder_2)
-    #decoder_2 = tf.keras.layers.ReLU()(decoder_2)  # ÀÌ·¸°ÔÇØµµ µÇ´Â°Ç°¡???
+    #decoder_2 = tf.keras.layers.ReLU()(decoder_2)  # ï¿½Ì·ï¿½ï¿½ï¿½ï¿½Øµï¿½ ï¿½Ç´Â°Ç°ï¿½???
 
     return tf.keras.Model(inputs=inputs, outputs=h)
