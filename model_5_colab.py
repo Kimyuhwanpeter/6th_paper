607f1913984721dc93efe07391c31359474d1ee1
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2022-05-04 22:51:11 +0900

    Update model_5_colab.py

--- model_5_colab.py
+++ model_5_colab.py
@@ -97,11 +97,11 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     
     h = inputs = tf.keras.Input(input_shape)
 
-    h_att_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False, name="conv1")(h)
+    h_att_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=True, name="conv1")(h)
     h_att_1 = tf.keras.layers.BatchNormalization()(h_att_1)
     h_att_1 = tf.keras.layers.ReLU()(h_att_1)
 
-    h_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=False, name="conv2")(h_att_1)
+    h_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", use_bias=True, name="conv2")(h_att_1)
     h_1 = tf.keras.layers.BatchNormalization()(h_1)
     h_1 = tf.keras.layers.ReLU()(h_1)
 
@@ -111,11 +111,11 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     # h = tf.keras.layers.ReLU()(h)
     h = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2)(h_1) # downsample
 
-    h_att_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False, name="conv3")(h)
+    h_att_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=True, name="conv3")(h)
     h_att_2 = tf.keras.layers.BatchNormalization()(h_att_2)
     h_att_2 = tf.keras.layers.ReLU()(h_att_2)
 
-    h_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=False, name="conv4")(h_att_2)
+    h_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", use_bias=True, name="conv4")(h_att_2)
     h_2 = tf.keras.layers.BatchNormalization()(h_2)
     h_2 = tf.keras.layers.ReLU()(h_2)
 
@@ -125,11 +125,11 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     # h = tf.keras.layers.ReLU()(h)
     h = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2)(h_2) # downsample
 
-    h_att_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False, name="conv5")(h)
+    h_att_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=True, name="conv5")(h)
     h_att_3 = tf.keras.layers.BatchNormalization()(h_att_3)
     h_att_3 = tf.keras.layers.ReLU()(h_att_3)
     
-    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False, name="conv6")(h_att_3)
+    h_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=True, name="conv6")(h_att_3)
     h_3 = tf.keras.layers.BatchNormalization()(h_3)
     h_3 = tf.keras.layers.ReLU()(h_3)
 
@@ -139,11 +139,11 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     # h = tf.keras.layers.ReLU()(h)
     h = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2)(h_3) # downsample
 
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding="same", use_bias=False, name="conv7")(h)
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding="same", use_bias=True, name="conv7")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU()(h)
 
-    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding="same", use_bias=False, name="conv8")(h)
+    h = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding="same", use_bias=True, name="conv8")(h)
     h = tf.keras.layers.BatchNormalization()(h)
     h = tf.keras.layers.ReLU()(h)
     encoder_2 = h
@@ -194,21 +194,19 @@ def modified_model(input_shape=(512, 512, 3), classes=2):
     h = tf.keras.layers.ReLU()(h)
 
     h = tf.keras.layers.Conv2D(filters=classes, kernel_size=1)(h) # object
-    decoder_output_1 = h
-    #########################################################################################################################
+    #decoder_output_1 = h
 
-    #encoder_2_1 = ASPP(encoder_2[:, :, :, 0:256], 256)
-    #encoder_2_1 = Upsample(encoder_2_1, [K.int_shape(encoder_2)[1] * 2, K.int_shape(encoder_2)[2] * 2])
+    model = tf.keras.Model(inputs=inputs, outputs=h)
+
+    backbone = tf.keras.applications.VGG16(input_shape=(224, 224, 3))
     
-    #encoder_2_2 = ASPP(encoder_2[:, :, :, 256:], 256)
-    #encoder_2_2 = Upsample(encoder_2_2, [K.int_shape(encoder_2)[1] * 2, K.int_shape(encoder_2)[2] * 2])
-
-    #decoder_2 = tf.concat([encoder_2_1, encoder_2_2], -1)
-    #decoder_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(decoder_2)
-    #decoder_2 = tf.keras.layers.BatchNormalization()(decoder_2)
-    #decoder_2 = tf.keras.layers.ReLU()(decoder_2)
-    #decoder_2 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", use_bias=False)(decoder_2)
-    #decoder_2 = tf.keras.layers.BatchNormalization()(decoder_2)
-    #decoder_2 = tf.keras.layers.ReLU()(decoder_2)  # ???????? ?????????
+    model.get_layer("conv1").set_weights(backbone.get_layer("block1_conv1").get_weights())
+    model.get_layer("conv2").set_weights(backbone.get_layer("block1_conv2").get_weights())
+    model.get_layer("conv3").set_weights(backbone.get_layer("block2_conv1").get_weights())
+    model.get_layer("conv4").set_weights(backbone.get_layer("block2_conv2").get_weights())
+    model.get_layer("conv5").set_weights(backbone.get_layer("block3_conv1").get_weights())
+    model.get_layer("conv6").set_weights(backbone.get_layer("block3_conv2").get_weights())
+    model.get_layer("conv7").set_weights(backbone.get_layer("block4_conv1").get_weights())
+    model.get_layer("conv8").set_weights(backbone.get_layer("block4_conv2").get_weights())
 
     return tf.keras.Model(inputs=inputs, outputs=h)
